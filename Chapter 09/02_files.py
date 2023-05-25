ca4474c2b2a4917bdb1bfa6a4298ffb832e06a7a
protoyroy <protoy.roy47@gmail.com>
2023-05-25 13:59:34 +0600

    New code

---
+++ Chapter 09/02_files.py
@@ -0,0 +1,16 @@
+##read first line
+f = open ('Chapter 09/sample.txt')
+data = f.readline()
+print(data)
+##read second line
+data = f.readline()
+print(data)
+##read third line
+data = f.readline()
+print(data)
+
+f.close()
+# f = open('Chapter 09/sample.txt', 'r')
+# data = f.readline()
+# print(data)
+# f.close()
