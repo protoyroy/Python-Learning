72c0e12b5cd0bd72e3d2e27b96c1105e80d31d81
protoyroy <protoy.roy47@gmail.com>
2023-05-31 11:22:49 +0600

    New

---
+++ Chapter 11/07_property_decorators.py
@@ -0,0 +1,18 @@
+class Employee:
+    company = "Bharat Gas"
+    salary = 30000
+    salarybonus = 3500
+    # totalSalary = 33500
+
+    @property    #to sum total salary (instead of change tottal salary every time we can do that) 
+    def totalSalary(self):
+        return self.salary + self.salarybonus
+    
+    @totalSalary.setter    #to change salary bonus according to total salary
+    def totalSalary(self, val):
+        self.salarybonus = val -self.salary
+e = Employee()
+print(e.totalSalary)
+e.totalSalary = 25000 #to change salary and salary bonus
+print(e.salary)
+print(e.salarybonus)
