c873d78d86c526d519135f81d08838ccebc75f0d
protoyroy <protoy.roy47@gmail.com>
2023-06-07 23:57:21 +0600

    Updated

--- Chapter 11/13_pr_04.py
+++ Chapter 11/13_pr_04.py
@@ -1,3 +1,6 @@
+#multiplication formula for complkex number
+#(a+bi)(c+di) = (ac-bd) +(ad+bc)i
+
 class ComplexNumber:
     def __init__(self, r , i):
         self.real = r
@@ -5,7 +8,10 @@ class ComplexNumber:
 
     def __add__(self, c):
         return complex(self.real+ c.real, self.imaginary + c.imaginary)
+    def __mul__(self, c):
+        return complex(self.real * c.real - self.imaginary * c.imaginary, self.real*c.imaginary - self.imaginary*c.real)
 
 c1 = ComplexNumber(1, 4) #c1 =complex(1,4) i can also write that as complex is a build in function in python
 c2 = ComplexNumber(8, 5) #c2 =complex(8,5)
 print(c1+c2)
+print(c1*c2)
